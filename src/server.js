f5c4a4465d8301463c78fa959234ee109bf3bcbd
RozhR <rozhroman@gmail.com>
2026-07-27 19:42:54 +0300

    Add database configuration and Sequelize models for Book, Author, and Publisher

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
-import express from 'express';
-import bookRoutes from './routes/book.routes.js';
-import config from "./configuration/config.js";
+import express from "express";
+import bookRoutes from "./routes/book.routes.js";
+import authorRoutes from "./routes/author.routes.js";
+import publisherRoutes from "./routes/publisher.routes.js";
+import errorHandler from "./middlewares/error.middleware.js";
+import {dbConnection} from "./config/database.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/', bookRoutes);
+app.use(bookRoutes);
+app.use(authorRoutes);
+app.use(publisherRoutes);
 
-app.listen(config.port, () => console.log(`Server running on port ${config.port}. Press Ctrl+C to stop.`));
+app.use(errorHandler);
+
+app.use((req, res) => res.status(404).type('text/plain; charset=utf-8').send('Not Found'));
+
+async function startServer() {
+    await dbConnection();
+    app.listen(process.env.PORT || 8080, () => console.log(`Server running on port ${process.env.PORT || 8080}. Press Ctrl+C to stop.`));
+}
+
+startServer();
