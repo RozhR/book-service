60228d4263696340eaff993eef400e7bd22b14ff
RozhR <rozhroman@gmail.com>
2026-08-05 23:25:27 +0300

    Refactor findBooksByPublisher to use repository pattern instead of association method

--- src/repositories/book.repository.js
+++ src/repositories/book.repository.js
@@ -4,8 +4,4 @@ export const addBook = async (book, options ={}) => Book.create(book, options);
 
 export const findBookById = async (id, options = {}) => await Book.findByPk(id, options);
 
-export const updateBookTitle = async (book, title, options = {}) => book.update({ title }, options);
-
-export const findBooksByAuthor = async (options = {}) => Book.findAll(options);
-
-export const findBooksByPublisher = async (options = {}) => Book.findAll(options);
+export const findBooks = async (options = {}) => await Book.findAll(options);
