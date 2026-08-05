26a9696544fa87164d366e8ea1e2adf5c32f87e3
RozhR <rozhroman@gmail.com>
2026-08-05 23:21:20 +0300

    Implement updateBookTitle service with transaction support and author associations

--- src/repositories/book.repository.js
+++ src/repositories/book.repository.js
@@ -3,3 +3,9 @@ import { Book } from '../models/index.js';
 export const addBook = async (book, options ={}) => Book.create(book, options);
 
 export const findBookById = async (id, options = {}) => await Book.findByPk(id, options);
+
+export const updateBookTitle = async (book, title, options = {}) => book.update({ title }, options);
+
+export const findBooksByAuthor = async (options = {}) => Book.findAll(options);
+
+export const findBooksByPublisher = async (options = {}) => Book.findAll(options);
